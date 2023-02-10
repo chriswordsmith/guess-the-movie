b23b9f84e5eb3eb7962af845134f2315aa18e175
MR CHRISTOPHER J SMITH <mrchristopherjsmith@MRs-MacBook-Pro.local>
2023-02-10 18:44:51 +0000

    added the buttons and some logic

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let poster = ""
 
+let correctMovie = ""
+let incorrectMovie = ""
+
+
  function displayMovieInfo() {
 
    var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=ca57f8d195555909f808f10b7ae17e17&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
@@ -17,14 +21,52 @@ let poster = ""
     $('#poster').append(imageBox)
     imageBox.addClass("imagesq")
     imageBox.attr('src', "https://image.tmdb.org/t/p/original/" + poster )
-    //$('#poster').append('<img></img>')
+  
+    $("#movie-1").text(movie.title)
+    
+    let randomMovieTwo = response.results[Math.floor(Math.random() * 20)];
+    let randomMovieTwoTitle = randomMovieTwo.title
+  
+    $("#movie-2").text(randomMovieTwoTitle)
 
-    //  var titleTd = $("<td>").text(response.Title);
- // var yearTd = $("<td>").text(response.Year);
- // var actorsTd = $("<td>").text(response.Actors);
-    //$('#poster').attr('src', "https://image.tmdb.org/t/p/original/" + movie)
-    //'<img src="/static/on.png" height="64px" width="64px">'
+    let randomMovieThree = response.results[Math.floor(Math.random() * 20)];
+    let randomMovieThreeTitle = randomMovieThree.title
+
+    $("#movie-3").text(randomMovieThreeTitle)
+
+    let randomMovieFour = response.results[Math.floor(Math.random() * 20)];
+    let randomMovieFourTitle = randomMovieFour.title
+    $("#movie-4").text(randomMovieFourTitle)
+
+// we'll need a bigger selection of movies AND do an operator for strict equality
+
+// randomising the button choices
+
+// logic of which movie number / button is correct 
+
+// on.("click") 
+
+// if buttonnumber === correctMovie, score++
+// else alert WRONG
+
+
+
    });
 
  }
  displayMovieInfo()
+
+//  // 
+//  function name() {
+  
+//  }
+
+
+//  .if (condition) {
+  
+//  } else {
+  
+//  }
+
+
+//  Math.random()
